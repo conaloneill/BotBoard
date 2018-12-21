6883aed20e320ffe69755072d3c7d9de7741c41e
conaloneill <conaloneill@users.noreply.github.com>
2018-12-21 13:28:01 +0000

    update gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import org.restlet.Component;
-import org.restlet.Server;
 import org.restlet.data.Protocol;
 
 public class Main {
@@ -10,8 +9,6 @@ public class Main {
 		component.getDefaultHost().
 				attach("", new HelloApplication());
 		component.start();
-//
-//		Server server = new Server(Protocol.HTTP, 9000);
-//		server.start();
+
 	}
 }
